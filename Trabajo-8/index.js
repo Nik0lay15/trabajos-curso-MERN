f47d706d799d96c3412b3159132191c73194c386
Nik0lay15 <manueltello41@gmail.com>
2021-09-03 21:54:32 -0300

    Act TP-8

--- Trabajo-8/index.js
+++ Trabajo-8/index.js
@@ -11,8 +11,18 @@ server.on("Error:",(error)=>{
     console.log(error);
 });
 
+//// Ruta productos
+app.get("/api/productos/listar",(req,res)=>{    
 
+});
+// " por ID
+app.get("/api/productos/listar/:id",(req,res)=>{
+    const parametro =  req.params;
+    console.log(`Nuevo request a ${req.path}:\n${lista_productos[parametro.id]}`);
+    res.json(lista_productos[parametro.id]);
+});
 
+//// Ruta incorpora producto
+app.post("/api/productos/guardar",(req,res)=>{
 
-
-
+});
