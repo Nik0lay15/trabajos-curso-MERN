27b18b9a769803147b915bcff76839c603f5cd4a
Nik0lay15 <manueltello41@gmail.com>
2021-09-10 22:52:09 -0300

    TP-10 añadido

---
+++ Trabajo-10/app.js
@@ -0,0 +1,32 @@
+// Dependencias
+import express from "express";                  // Express
+import router from "./routes/api.js";           // Ruteo
+import handlebars from "express-handlebars";    // MVC handlebars
+
+// Globales
+const app = express();
+const PUERTO = 8080;
+const server = app.listen(PUERTO,()=>{
+    console.log(`Escuchando localhost:${PUERTO}`);
+})
+server.on("Error:",(error)=>{
+    console.log(error);
+});
+
+// Midleware
+app.use(express.json());                        
+app.use(express.urlencoded({extended:true}));   
+app.use(express.static("./public"));                
+app.use("/api",router);
+app.use("/css",express.static("./node_modules/bootstrap/dist/css"));
+app.engine("hbs",handlebars({
+        extname : ".hbs",
+        defaultLayout : "base.hbs",
+        layoutsDir : "./views/layouts",
+        partialsDir : "./views/partials"
+    }
+));
+app.set("views","./views");
+app.set("view engine","hbs");
+
+
