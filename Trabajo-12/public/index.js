69d8d75c712a2c60fbcbef4b07d5467c9cbfb79e
Nik0lay15 <manueltello41@gmail.com>
2021-09-22 14:56:21 -0300

    Icono de productos agregado

--- Trabajo-12/public/index.js
+++ Trabajo-12/public/index.js
@@ -2,24 +2,34 @@ const socket = io();
 
 const enviarMensaje = ()=>{
     const inputs = document.getElementsByClassName("form-control");
-    const payload = [
-        document.getElementsByClassName("entrada").length,      // ID
-        inputs[0].value,                                        // Title
-        inputs[1].value,                                        // Price
-        inputs[2].value                                         // Thumbail
-    ];
+    const payload = {    
+        values : [
+            document.getElementsByClassName("entrada").length,      // ID
+            inputs[0].value,                                        // Title
+            inputs[1].value                                         // Price
+        ],
+        img : inputs[2].value
+    };
+
     socket.emit("cliente",payload);
 }; 
 
 socket.on("broadcast",(data)=>{
     let tbody = document.getElementsByTagName("tbody")[0];
     let tr = document.createElement("tr");
-    
-    for(let item of data){
+    let td_img = document.createElement("td");
+    let img = document.createElement("img");
+    img.src = data.img;
+    img.width = 30;
+    img.class = "rounded";
+    td_img.appendChild(img);
+    tr.className = "entrada";
+
+    for(let item of data.values){
         const td = document.createElement("td");
         td.innerHTML = item;
         tr.appendChild(td);
     }
-    tr.className = "entrada";
+    tr.appendChild(td_img);
     tbody.appendChild(tr);
 });
