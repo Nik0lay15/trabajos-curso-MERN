0781684936359223f25368648ae417d000a0d07f
Manuel <manueltello41@gmail.com>
2021-10-05 11:24:21 -0300

    Query actualizado

--- Trabajo-16/productos.sql
+++ Trabajo-16/productos.sql
@@ -1,6 +1,7 @@
-create database prueba;
-use prueba;
-create table item (
+CREATE DATABASE prueba IF NOT EXISTS;
+
+USE prueba;
+CREATE TABLE item (
   	id int(40) auto_increment not null,
 	nombre varchar(40) not null,
   	categoria varchar(10) not null,
@@ -8,6 +9,14 @@ create table item (
   	primary key(id)
 );
 
-insert into item(nombre,categoria,stock) values("Fideos","Harina",20);
-insert into item(nombre,categoria,stock) values("Leche","Lacteos",30);
-insert into item(nombre,categoria,stock) values("Crema","Lacteos",15);
+INSERT INTO item(nombre,categoria,stock) VALUES("Fideos","Harina",20);
+INSERT INTO item(nombre,categoria,stock) VALUES("Leche","Lacteos",30);
+INSERT INTO item(nombre,categoria,stock) VALUES("Crema","Lacteos",15);
+
+SELECT * FROM item;
+
+DELETE FROM item WHERE id = 1;
+
+UPDATE item SET stock = 45 WHERE id = 2; 
+
+SELECT * FROM item WHERE id != 1 AND stock = 45;
